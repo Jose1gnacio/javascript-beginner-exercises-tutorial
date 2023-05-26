bd36bfbf596fd57a5bd479dddec04c2c568bb068
Jose1gnacio <104277952+Jose1gnacio@users.noreply.github.com>
2023-05-26 19:29:35 +0000

    exercises1 js

--- exercises/04-User_inputed_variables/app.js
+++ exercises/04-User_inputed_variables/app.js
@@ -1,3 +1,10 @@
 let age = prompt('What is your age?');
 
 // Your code below:
+let a = 10;
+let sum = Number(age) + a;
+// por que las sumas de abajo no funcionan?
+// let sum = age + Number(10)
+// let sum = age + 10
+// let sum = `${Number(age) + Number(10)}`;
+console.log(sum)
